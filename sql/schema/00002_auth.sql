a50a188adcc23fd267623805654527901c8703be
John <128350391+alpaOo@users.noreply.github.com>
2025-10-26 23:56:12 +0800

    refactor(db): update schema and reorder migrations
    A new auth.sql file has been added, which creates migrations for the refresh_tokens table. The migration order is updated and auth.sql sits in between users and messages

---
+++ sql/schema/00002_auth.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE refresh_tokens (
+  token VARCHAR NOT NULL PRIMARY KEY,
+  created_at TIMESTAMPTZ NOT NULL,
+  updated_at TIMESTAMPTZ,
+  user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  expires_at TIMESTAMPTZ NOT NULL,
+  revoked_at TIMESTAMPTZ
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE refresh_tokens;
+-- +goose StatementEnd
