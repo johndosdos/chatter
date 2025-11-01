65ea39143e3c0dc70d448cfcfe439a59c1cb5dd7
John <128350391+alpaOo@users.noreply.github.com>
2025-11-01 23:19:04 +0800

    refactor(auth.go): Add comments to exported functions, variables, and structs

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,3 +1,5 @@
+// Package auth provides functions related to password hashing and session
+// tokens.
 package auth
 
 import (
@@ -16,19 +18,26 @@ import (
 	"github.com/johndosdos/chatter/internal/database"
 )
 
+// The ContextKey type is meant for passing userID as key for
+// context.WithValue.
 type ContextKey string
 
-const UserIdKey ContextKey = "userId"
+// UserIDKey implements the ContextKey type.
+const UserIDKey ContextKey = "userId"
 
+// HashPassword returns the hashed password created using the argon2id
+// package.
 func HashPassword(password string) (string, error) {
-	hashed_pw, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	hashedPw, err := argon2id.CreateHash(password, argon2id.DefaultParams)
 	if err != nil {
 		return "", fmt.Errorf("internal/auth: pw hash failed: %w", err)
 	}
 
-	return hashed_pw, nil
+	return hashedPw, nil
 }
 
+// CheckPasswordHash compares the password and the hash. It returns true
+// when they match, otherwise it returns false.
 func CheckPasswordHash(password, hash string) (bool, error) {
 	isMatch, err := argon2id.ComparePasswordAndHash(password, hash)
 	if err != nil {
@@ -41,11 +50,13 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	return isMatch, nil
 }
 
-func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+// MakeJWT returns a JSON Web Token string to be used as an acess token
+// for client session.
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
 	now := time.Now().UTC()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
 		Issuer:    os.Getenv("JWT_ISS"),
-		Subject:   userId.String(),
+		Subject:   userID.String(),
 		IssuedAt:  jwt.NewNumericDate(now),
 		ExpiresAt: jwt.NewNumericDate(now.Add(expiresIn)),
 	})
@@ -53,12 +64,14 @@ func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 	return token.SignedString([]byte(tokenSecret))
 }
 
+// ValidateJWT tries to validate the access token. It returns the user id
+// as a uuid.UUID type. The returned error is a uuid.Parse error.
 func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 	claims := &jwt.RegisteredClaims{}
 	token, err := jwt.ParseWithClaims(
 		tokenString,
 		claims,
-		func(t *jwt.Token) (any, error) { return []byte(tokenSecret), nil },
+		func(*jwt.Token) (any, error) { return []byte(tokenSecret), nil },
 		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}),
 		jwt.WithExpirationRequired(),
 	)
@@ -78,6 +91,8 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 	return uuid.Parse(userid)
 }
 
+// MakeRefreshToken returns a refresh token string, while also storing the
+// token to the database.
 func MakeRefreshToken(ctx context.Context, db *database.Queries) (string, error) {
 	rnd := make([]byte, 32)
 
@@ -85,12 +100,12 @@ func MakeRefreshToken(ctx context.Context, db *database.Queries) (string, error)
 	_, _ = rand.Read(rnd)
 	rndStr := hex.EncodeToString(rnd)
 
-	userId := ctx.Value(UserIdKey).(uuid.UUID)
+	userID := ctx.Value(UserIDKey).(uuid.UUID)
 	refreshTokenExp := time.Now().UTC().AddDate(0, 0, 7)
 	refreshToken, err := db.CreateRefreshToken(ctx, database.CreateRefreshTokenParams{
 		Token:     rndStr,
 		CreatedAt: pgtype.Timestamptz{Time: time.Now().UTC(), Valid: true},
-		UserID:    pgtype.UUID{Bytes: userId, Valid: true},
+		UserID:    pgtype.UUID{Bytes: userID, Valid: true},
 		ExpiresAt: pgtype.Timestamptz{Time: refreshTokenExp, Valid: true},
 	})
 	if err != nil {
