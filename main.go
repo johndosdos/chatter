513f51381a32cf05d3feab10e011a3ff13695208
John <128350391+alpaOo@users.noreply.github.com>
2026-01-08 22:34:39 +0800

    refactor: prepare main.go for google cloud run integration
    - Use joho/godotenv to load env files instead of dotenvx. godotenv is easier to implement when deploying to the cloud.
    - Improve message logs for clarity and easier debugging.
    
    [SERVER]
    - Create dedicated ServeMux for HTTP handlers instead of polluting the default global ServeMux.
    - Improve signal cancellation handler for graceful server shutdowns while also adding a 10 second timeout.
    - Initialize server first before listening for DB and NATS connection.
    
    [NATS]
    - Improve NATS credential retrieval logic.
    - Update NATS jetsream config to only store at most 1 GB of data.
    - Add 5 second connection timeout when connecting to the NATS URL.

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nats.go/jetstream"
 
@@ -23,83 +24,118 @@ import (
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("failed to load .env file: %+v", err)
+	}
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetOutput(os.Stdout)
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
+	// Init server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	server := &http.Server{
+		Addr:              "0.0.0.0:" + port,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       15 * time.Second,
+	}
+
+	go func() {
+		log.Printf("Server starting at 0.0.0.0:%s", port)
+		log.Fatal(server.ListenAndServe())
+	}()
+
 	// Init NATS
+	log.Println("Starting application...")
+	log.Println("Initializing NATS connection...")
+
 	var natsCredentials []nats.Option
+
 	natsURL := os.Getenv("NATS_URL")
+	if natsURL == "" {
+		log.Fatal("NATS_URL environment variable is not set")
+	}
 
 	if cred := os.Getenv("NATS_CRED"); cred != "" {
 		natsCredentials = append(natsCredentials, nats.UserCredentials(cred))
-	} else {
-		natsCredentials = append(natsCredentials, nats.UserInfo(os.Getenv("NATS_USER"), os.Getenv("NATS_PASSWORD")))
+	} else if user, pass := os.Getenv("NATS_USER"), os.Getenv("NATS_PASSWORD"); user != "" && pass != "" {
+		natsCredentials = append(natsCredentials, nats.UserInfo(user, pass))
 	}
+
+	natsCredentials = append(natsCredentials, nats.Timeout(5*time.Second))
 
 	conn, err := nats.Connect(natsURL, natsCredentials...)
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Fatalf("failed to connect to nats: %v", err)
 	}
 	defer conn.Drain()
 
 	js, err := jetstream.New(conn)
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Fatalf("failed to create jetstream instance: %v", err)
 	}
 
 	stream, err := js.CreateOrUpdateStream(ctx, jetstream.StreamConfig{
 		Name:     broker.StreamName,
 		Subjects: []string{broker.SubjectGlobalRoom},
+		MaxBytes: 1 << 30,
 	})
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Fatalf("failed to create/update stream: %v", err)
 	}
 
 	// Init DB
+	log.Println("Initializing Database connection...")
+
 	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
 	dbConn, err := pgxpool.New(ctx, dbURL)
 	if err != nil {
 		log.Fatalf("could not connect to the postgresql database: %v", err)
 	}
+
 	dbQueries := database.New(dbConn)
 
-	// hub.Run is our central hub that is always listening for client related
-	// events.
+	// Init Hub
 	hub := chat.NewHub(js, dbQueries)
 	go hub.Run(ctx, stream)
 
-	port, found := os.LookupEnv("PORT")
-	if !found {
-		port = "8080"
-	}
-	server := &http.Server{
-		Addr:              ":" + port,
-		ReadHeaderTimeout: 5 * time.Second,
-		ReadTimeout:       15 * time.Second,
-	}
-
+	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.Handle("/account/login", handler.ServeLogin(dbQueries))
-	http.Handle("/account/signup", handler.ServeSignup(dbQueries))
-	http.Handle("/account/logout", handler.ServeLogout(dbQueries))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.Handle("/account/login", handler.ServeLogin(dbQueries))
+	mux.Handle("/account/signup", handler.ServeSignup(dbQueries))
+	mux.Handle("/account/logout", handler.ServeLogout(dbQueries))
 
 	// Load chat history on HTTP GET on initial connection before starting websockets.
 	// This is to prevent issues regarding resending chat history on websocket reconnection.
-	http.Handle("/messages", internal.Middleware(handler.ServeMessages(dbQueries), dbQueries))
-	http.Handle("/stream", internal.Middleware(handler.StreamSSE(hub, dbQueries), dbQueries))
-	http.Handle("/chat", internal.Middleware(handler.ServeChat(), dbQueries))
-	http.Handle("/send", internal.Middleware(chat.Send(hub, dbQueries), dbQueries))
+	mux.Handle("/messages", internal.Middleware(handler.ServeMessages(dbQueries), dbQueries))
+	mux.Handle("/stream", internal.Middleware(handler.StreamSSE(hub, dbQueries), dbQueries))
+	mux.Handle("/chat", internal.Middleware(handler.ServeChat(), dbQueries))
+	mux.Handle("/send", internal.Middleware(chat.Send(hub, dbQueries), dbQueries))
 
-	http.Handle("/", handler.ServeRoot())
+	mux.Handle("/", handler.ServeRoot())
 
-	go func(server *http.Server) {
-		log.Println("Server starting at port", server.Addr)
-		log.Fatal(server.ListenAndServe())
-	}(server)
+	server.Handler = mux
 
 	<-ctx.Done()
 	log.Printf("Shutdown signal received; shutting down...")
+
+	shutdownCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Server stopped")
 }
