113e189e37626c994cfed3caa3a228a210b99ccd
John <128350391+alpaOo@users.noreply.github.com>
2026-01-10 00:13:54 +0800

    refactor:
    - Update 'Publisher' method to not return ack sequence ID. We'll use 'msg.Metadata' from the Subscriber for our sequence ID.
    Note: Sequence ID for last-event-id header is not implemented. We'll let the clients handle missed messages by refreshing the app.
    
    - Add 'jetstream.ConsumerConfig' options to enable a more durable queue while also improving logic flow.

--- internal/broker/pubsub.go
+++ internal/broker/pubsub.go
@@ -5,40 +5,47 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/johndosdos/chatter/internal/model"
 	"github.com/nats-io/nats.go/jetstream"
 )
 
-func Publisher(ctx context.Context, js jetstream.JetStream, payload model.Message) (uint64, error) {
+func Publisher(ctx context.Context, js jetstream.JetStream, payload model.Message) error {
 	if js == nil {
-		return 0, fmt.Errorf("jetstream interface is nil")
+		return fmt.Errorf("jetstream interface is nil")
 	}
 	if ctx == nil {
-		return 0, fmt.Errorf("context is nil")
+		return fmt.Errorf("context is nil")
 	}
 
 	p, err := json.Marshal(payload)
 	if err != nil {
-		return 0, fmt.Errorf("could not encode payload to JSON: %w", err)
+		return fmt.Errorf("could not encode payload to JSON: %w", err)
 	}
 
-	pubAck, err := js.Publish(ctx,
+	_, err = js.Publish(ctx,
 		SubjectGlobalRoom,
 		p,
 		jetstream.WithMsgID(uuid.NewString()),
 	)
 	if err != nil {
-		return 0, fmt.Errorf("failed to publish to stream [%s]: %v", SubjectGlobalRoom, err)
+		return fmt.Errorf("failed to publish to stream [%s]: %v", SubjectGlobalRoom, err)
 	}
 	log.Printf("publish successful from [%s]\n", payload.Username)
 
-	return pubAck.Sequence, nil
+	return nil
 }
 
 func Subscriber(ctx context.Context, stream jetstream.Stream, receiveMsg chan model.Message) error {
-	consumer, err := stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{})
+	consumer, err := stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+
+		Durable:    "chat_consumer",
+		AckPolicy:  jetstream.AckExplicitPolicy,
+		AckWait:    30 * time.Second,
+		MaxDeliver: 3,
+	})
 	if err != nil {
 		return fmt.Errorf("failed to create or update consumer: %w", err)
 	}
@@ -48,16 +55,24 @@ func Subscriber(ctx context.Context, stream jetstream.Stream, receiveMsg chan mo
 
 		err := json.Unmarshal(msg.Data(), &payload)
 		if err != nil {
-			_ = msg.Term()
 			log.Printf("could not decode payload: %v", err)
+			if err := msg.Term(); err != nil {
+				log.Printf("could not terminate message: %v", err)
+				return
+			}
+			return
 		}
 
-		if err := msg.Ack(); err != nil {
-			log.Printf("failed to acknowledge message: %+v", err)
-		}
+		// metadata, err := msg.Metadata()
+		// if err == nil {
+		// 	payload.SequenceID = metadata.Sequence.Stream
+		// }
 
 		select {
 		case receiveMsg <- payload:
+			if err := msg.Ack(); err != nil {
+				log.Printf("failed to acknowledge message: %+v", err)
+			}
 		default:
 			log.Println("skipping message payload")
 		}
