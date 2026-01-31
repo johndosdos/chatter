1a1f08ec9df595072862dcec678f5554ca40a782
John <128350391+alpaOo@users.noreply.github.com>
2026-02-01 01:18:51 +0800

    Untrack internal/broker. For archival

--- internal/broker/routing.go
+++
@@ -1,7 +0,0 @@
-package broker
-
-// Using NATS is simpler than RabbitMQ for the projects requirements.
-var (
-	StreamName        = "MESSAGES"
-	SubjectGlobalRoom = StreamName + "." + "room.global"
-)
